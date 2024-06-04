f61fe30a04c3bad7353e2c139594779456f429ae
Patrick Ryan <patrick@Patricks-MacBook-Pro>
2024-06-04 19:25:13 -0400

    working on assignment 2

--- scripts/display.js
+++ scripts/display.js
@@ -1,24 +1,3 @@
-// function displayCards(){
-//     //declare variables
-//     let card="";
-//     const petDiv=document.getElementById("petDiv");
-//     //travel the array of pets
-//     for(let i=0;i<petSalon.pets.length;i++){
-//         let pet = petSalon.pets[i]        
-//         //create html template
-//         card+=`
-//             <div class="petCard">
-//                 <h4>${pet.name}</h4>
-//                 <p>${pet.type}<p>
-//                 <p>Age: ${pet.age}</p>
-//                 <p>${pet.gender}</p>
-//                 <p>${ pet.service}</p>
-//                 </div>
-//             `; 
-//     }
-//     petDiv.innerHTML=card; 
-//     //insert the tmp into HTML
-// }
 
 
 function displayRow(){
--- scripts/service.js
+++ scripts/service.js
@@ -1,4 +1,15 @@
 // Use JQuery
+let petSalon ={               //obj
+    name:"Suds and Scrubs",     //var
+    phone: "203 555 1234",      //var
+    hours:{
+        open:"9:00 am",       //var
+        close:"5:00 pm"       //var
+    },
+    pets:[]                   //array
+}
+
+
 
 function Service(description,price){
     this.description=description;
@@ -13,6 +24,10 @@ function isValid(service){
         validation=false;
         $("#txtDescription").addClass("alert-error");
 }
+    if(service.price==""){
+        validation=false;
+        $("#txtPrice").addClass("alert-error");
+    }
 
     return validation;
 }
@@ -31,4 +46,48 @@ function register(){
         console.log(newService);
         $("input").val("");
         }
-}
+        if (isValid(newService.description)){
+            petSalon.service.push(newService);
+            displayNewServiceRow();
+        }
+    }
+
+
+    function deleteService(index){
+        service.pets.splice(index,1);
+        displayNewServiceRow();
+    }
+    
+
+        function displayNewServiceRow(){
+            let table="";
+            const serviceTable=document.getElementById("newServiceTable");
+            for(let i=0;i<petSalon.service.length;i++){
+                let pet = petSalon.service[i]        
+                table+=`
+                    <tr>
+                        <th>${pet.description}</th>
+                        <th>${pet.price}</th>
+                        <th><button class="btn btn-danger" onclick="deleteService(${i})">Delete</button></th>
+                        </tr>
+                    `;
+            }
+            serviceTable.innerHTML=table;
+        }
+    
+
+
+        function init(){
+            let newService1 = new Service("Nail Trim",20); //declaring obj
+            let newService2 = new Service("Dental Clean",100); //declaring obj
+            petSalon.service.push(newService1);
+            petSalon.service.push(newService2);
+            displayNewServiceRow();
+        }
+
+
+
+
+
+
+        window.onload=init;  //waiting to render html
